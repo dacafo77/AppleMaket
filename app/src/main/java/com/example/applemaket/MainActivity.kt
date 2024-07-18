4e7305ae6c2cd1182fd399ffffa56256e8b54750
dacafo <dacafo77@nate.com>
2024-07-18 14:19:28 +0900

    메인페이지에 리사이클링뷰 적용

--- app/src/main/java/com/example/applemaket/MainActivity.kt
+++ app/src/main/java/com/example/applemaket/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.example.applemaket
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.applemaket.databinding.ActivityMainBinding
+import com.example.applemaket.main.ArticleAdapter
+import com.example.applemaket.main.ArticleModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var articleAdapter: ArticleAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        articleAdapter = ArticleAdapter()
+        articleAdapter.submitList(mutableListOf<ArticleModel>().apply {
+            add(ArticleModel("가방", "방배동", 50000, R.drawable.sample3))
+            add(ArticleModel("지갑", "대명동", 150000, R.drawable.sample2))
+        })
+
+        binding.articleRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.articleRecyclerView.adapter = articleAdapter
     }
 }
--- app/src/main/java/com/example/applemaket/main/ArticleAdapter.kt
+++ app/src/main/java/com/example/applemaket/main/ArticleAdapter.kt
@@ -1,11 +1,47 @@
 package com.example.applemaket.main
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.example.applemaket.databinding.ItemArticleBinding
+import java.text.SimpleDateFormat
 
 class ArticleAdapter : ListAdapter<ArticleModel, ArticleAdapter.ViewHolder>(diffUtil) {
 
     inner class ViewHolder(private val binding: ItemArticleBinding) :
         RecyclerView.ViewHolder(binding.root) {
+
+            fun bind(articleModel: ArticleModel) {
+                val formatprice = "${articleModel.pric}원"
+
+
+                binding.nameTextView.text = articleModel.name
+                binding.addressTextView.text = articleModel.address
+                binding.pricTextView.text = formatprice
+                binding.thumbnailImageView.setImageResource(articleModel.imageUrl)
+
+            }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(ItemArticleBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(currentList[position])
+    }
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<ArticleModel>(){
+            override fun areItemsTheSame(oldItem: ArticleModel, newItem: ArticleModel): Boolean {
+                return oldItem.name == newItem.name
+            }
+
+            override fun areContentsTheSame(oldItem: ArticleModel, newItem: ArticleModel): Boolean {
+                return oldItem == newItem
+            }
+
+        }
     }
 }
