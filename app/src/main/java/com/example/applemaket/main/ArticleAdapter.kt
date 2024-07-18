a8d6965376ffac257d3cc7923efdc0c30743f5d0
dacafo <dacafo77@nate.com>
2024-07-18 12:34:15 +0900

    ArticleModel 생성

--- app/src/main/java/com/example/applemaket/main/ArticleAdapter.kt
+++ app/src/main/java/com/example/applemaket/main/ArticleAdapter.kt
@@ -1,4 +1,10 @@
 package com.example.applemaket.main
 
-class ArticleAdapter {
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+class ArticleAdapter : ListAdapter<ArticleModel, ArticleAdapter.ViewHolder>(diffUtil){
+
+    inner class ArticlItemViewHolder(private val binding: ItemArticleBinding): RecyclerView.ViewHolder(binding.root)
+
 }
---
+++ app/src/main/java/com/example/applemaket/main/ArticleModel.kt
@@ -0,0 +1,8 @@
+package com.example.applemaket.main
+
+data class ArticleModel (
+    val name: String,
+    val address: String,
+    val pric: Int = 0,
+    val imageUrl: Int
+)
